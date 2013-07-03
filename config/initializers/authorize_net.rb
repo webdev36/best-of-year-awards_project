660dc3fe9e98f9d7007376df423352bbfc905abb
Greg Leuch <contact@gleu.ch>
2013-07-03 10:09:26 -0400

    heh. duh. fix.

--- config/initializers/authorize_net.rb
+++ config/initializers/authorize_net.rb
@@ -3,7 +3,7 @@ authorize_net_settings = YAML::load(File.open("#{Rails.root.to_s}/config/gateway
 ActiveMerchant::Billing::Base.mode = (['production'].include?(Rails.env) ? :production : :test)
 
 ::GATEWAY = ActiveMerchant::Billing::AuthorizeNetGateway.new(
-  :login    => authorize_net_settings[Rails.env]['login_id'] rescue '',
-  :password => authorize_net_settings[Rails.env]['transaction_key'] rescue '',
+  :login    => (authorize_net_settings[Rails.env]['login_id'] rescue ''),
+  :password => (authorize_net_settings[Rails.env]['transaction_key'] rescue ''),
   :test     => (!['production'].include?(Rails.env))
 )
